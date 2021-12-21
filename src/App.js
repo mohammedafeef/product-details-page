60caa911e661ccee5d65199973263bc0d62c5de1
afeef <afeef@sanoft.com>
2021-12-21 20:07:55 +0530

    feat: app complition * counter function * design updated * alignment adjusted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 // productDetails which is given
@@ -6,9 +6,11 @@ const ProductDetails = [
   { name: "product_id", value: "1" },
   { name: "store", value: "1" },
 ];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
   const [productDetails, setProductDetails] = useState();
+  const [selectedImage, setSelectedImage] = useState(0);
   const generateFormData = (data) => {
     const formData = new FormData();
     data.forEach((item) => formData.append(item.name, item.value));
@@ -20,37 +22,53 @@ function App() {
         method: "POST",
         body: generateFormData(ProductDetails),
         headers: {
-          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json",
         },
       });
       const product = await response.json();
-      console.log(resposnse, product);
+      setProductDetails(product.data);
     };
     fetchProductDetails();
   }, []);
-  const handleDecreaseCount = setCount(count--);
-  const handleIncreaseCount = setCount(count++);
+  const handleDecreaseCount = () => setCount(count > 1 ? count - 1 : 0);
+  const handleIncreaseCount = () => setCount(count + 1);
+  const changeProductImage = (index) => setSelectedImage(index);
   return (
     <ProductWrapper>
-      <ImagesWrapper>
-        <ImageList>
-          <Image src="https://via.placeholder.com/150" />
-        </ImageList>
-        <SelectedImageContainer>
-          <SelectedImage></SelectedImage>
-        </SelectedImageContainer>
-      </ImagesWrapper>
-      <Details>
-        <Name></Name>
-        <ProductSku></ProductSku>
-        <Describtion></Describtion>
-        <Price></Price>
-        <Quantity>
-          <Counter onClick={handleDecreaseCount}>-</Counter>
-          <Count>{count}</Count>
-          <Counter onClick={handleIncreaseCount}>+</Counter>
-        </Quantity>
-      </Details>
+      {productDetails && (
+        <>
+          <ImagesWrapper>
+            <ImageList>
+              {productDetails.images.map((image, index) => (
+                <Image
+                  src={image.images}
+                  onClick={() => changeProductImage(index)}
+                />
+              ))}
+            </ImageList>
+            <SelectedImageContainer>
+              <SelectedImageView
+                src={productDetails.images[selectedImage].images}
+              />
+            </SelectedImageContainer>
+          </ImagesWrapper>
+          <Details>
+            <Name>{productDetails.name}</Name>
+            <ProductSku>{productDetails.sku}</ProductSku>
+            <Describtion>{productDetails.description}</Describtion>
+            <Price>₹{productDetails.price}</Price>
+            <Quantity>
+              <Counter onClick={handleDecreaseCount} danger>
+                -
+              </Counter>
+              <Count>{count}</Count>
+              <Counter onClick={handleIncreaseCount} success>
+                +
+              </Counter>
+            </Quantity>
+          </Details>
+        </>
+      )}
     </ProductWrapper>
   );
 }
@@ -58,9 +76,10 @@ function App() {
 const ProductWrapper = styled.div`
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
   display: flex;
   flex-wrap: wrap;
-  padding: 3em;
+  padding: 2em;
 `;
 // image section
 const ImagesWrapper = styled.div`
@@ -74,14 +93,21 @@ const ImageList = styled.div`
   flex-direction: column;
 `;
 const Image = styled.img`
-  width: 30%;
-  height: 5em;
+  width: 70%;
+  height: 6em;
+  margin: 0.5em 0;
+  cursor: pointer;
+  transition: opacity 0.15s ease-in-out;
+  :hover{
+    opacity: 0.9;
+  }
+
 `;
 const SelectedImageContainer = styled.div`
   width: 70%;
   height: 100%;
 `;
-const SelectedImage = styled.img`
+const SelectedImageView = styled.img`
   width: 100%;
   height: 100%;
 `;
@@ -96,7 +122,7 @@ const Details = styled.div`
 const Name = styled.div`
   font-size: 2em;
   font-weight: bold;
-  padding: 1em 0;
+  padding: 1em 0 0;
 `;
 
 const ProductSku = styled.div`
@@ -108,11 +134,11 @@ const ProductSku = styled.div`
 
 const Describtion = styled.div`
   font-size: 1.5em;
-  padding: 1em 0;
+  padding: 1em 0 0;
 `;
 
 const Price = styled.div`
-  font-size: 1.5em;
+  font-size: 2em;
   font-weight: bold;
   padding: 1em 0;
 `;
@@ -121,13 +147,28 @@ const Quantity = styled.div`
   padding: 1em 0;
 `;
 const Counter = styled.button`
-  padding: 1em;
+  font-size: 2em;
+  font-weight: bolder;
+  padding: 0.2em 0.5em;
   border-radius: 50%;
   border: none;
+  background: ${(props) =>
+    props.success
+      ? "rgba(0,200,0,.4)"
+      : props.danger
+      ? "rgba(200,0,0,.4)"
+      : "white"};
+  transition: transform 0.2s ease-in-out;
+  &:hover {
+    transform: scale(1.05);
+  }
+  &:active {
+    transform: scale(0.96);
+  }
 `;
 const Count = styled.div`
   padding: 0 1em;
-  font-size: 1.5em;
+  font-size: 2em;
   font-weight: bold;
 `;
 
